db6eaaa55b66ddf9584d893b7d451a934a5b1330
Gerardo Veltri <gerardo.veltri@gmail.com>
2016-02-14 19:52:56 -0500

    back to productions server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server '52.36.55.16', user: 'deploy', roles: %w{web app db}
+server '52.27.230.200', user: 'deploy', roles: %w{web app db}
 
 
 # role-based syntax
